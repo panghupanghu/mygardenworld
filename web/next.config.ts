7a407db49f99e5e14c60e6406f67269d7dc5cbe3
胖虎 <89387263@qq.com>
2026-08-30 09:16:33 +0800

    自用配置CDN编译

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
-
+const CDN_PREFIX =
+  process.env.CDN_PREFIX ??
+  "http://static.xbase.cloud/file/35tlcfgsqfws0i4m8v8/xyd";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
   output: "export",
+  assetPrefix: CDN_PREFIX.replace(/\/$/, ""),
 };
 
 export default nextConfig;
